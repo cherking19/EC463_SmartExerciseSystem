ef0cf6c02ab55450229c872eea9fea133f0fa79a
Benjamin Marin Canseco <benmc@crc-dot1x-nat-10-239-151-191.bu.edu>
2022-11-30 14:19:44 -0500

    Added Ios

--- smart_gym/lib/main.dart
+++ smart_gym/lib/main.dart
@@ -31,9 +31,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(
-          title:
-              'Smart Gym'), //const SignInScreen(), //MyHomePage(title: 'Smart Gym'),//
+      home: const SignInScreen(), //MyHomePage(title: 'Smart Gym'),//
     );
   }
 }
