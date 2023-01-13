8287cf50c52b6c4301f98169f295ebb3727cd119
Carlton Knox <cknox@bu.edu>
2023-01-13 03:05:23 -0500

    added code to control haptic motor, and circuit image

--- arduino_powercontrol/powertest/powertest.ino
+++ arduino_powercontrol/powertest/powertest.ino
@@ -1,18 +1,34 @@
 #define powerdrain 12
+#define haptic 13
 unsigned long start_time;
 void setup() {
   // put your setup code here, to run once:
   pinMode(powerdrain, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(haptic, OUTPUT);
+//  pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(powerdrain, LOW);
-  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(haptic, LOW);
+//  digitalWrite(LED_BUILTIN, LOW);
+  Serial.begin(9600);
   start_time = millis();
+  
+
+  //counter
+  
+
+  //endcounter
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if (millis() - start_time >= 3000){
     digitalWrite(powerdrain, HIGH);
-    digitalWrite(LED_BUILTIN, HIGH);
+//    digitalWrite(LED_BUILTIN, HIGH);
   }
+  if(millis()%1000 < 400)
+    digitalWrite(haptic,HIGH);
+  else
+    digitalWrite(haptic,LOW);
+  Serial.print(String(millis()));
+  Serial.print("\n");
 }
