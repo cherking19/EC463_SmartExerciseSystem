8cb4799d7eac84388cff97471b6fd372e5eeae02
Carlton Knox <cknox@bu.edu>
2023-01-31 15:55:24 -0500

    Added comments

--- read_exercise.py
+++ read_exercise.py
@@ -14,7 +14,7 @@ state=0
 #0 = calibration
 #1 = curling
 while(True):
-    if sensor.in_waiting > 0:
+    if sensor.in_waiting > 0: # if there is data, read it
         # Read data out of the buffer until a carraige return / new line is found
         serialString = sensor.readline()
         
@@ -32,10 +32,10 @@ while(True):
         if((now-start)>.1):
             # print(".1 second has passed")
             start=timer()
-            pyav,ppav,prav= yav,pav,rav
+            pyav,ppav,prav= yav,pav,rav # previous values, to check for movement
             yav,pav,rav = fmean(yaws),fmean(pitches),fmean(rolls)            
             
-            if(state==0):
+            if(state==0):# calibration state
                 print(yav,pav,rav)
                 if(abs(yav-pyav)<1 and abs(rav-prav)<1 and abs(pav-ppav)<1):
                     still=still+1
@@ -44,24 +44,24 @@ while(True):
                     print("calibration complete, starting point = ",yav,pav,rav)
                     state=1
                     curls=0
-            elif(state==1):
+            elif(state==1):# after calibration, down state
                 dy,dp,dr=abs(yav-start_y),abs(pav-start_p),abs(rav-start_r)
                 
                 # print(dy,dp,dr)
                 if(dp>60):
-                    state=2
+                    state=2 #up state
                 holdup=0
-            elif(state==2):
+            elif(state==2): # up state
                 dy,dp,dr=abs(yav-start_y),abs(pav-start_p),abs(rav-start_r)
                 holdup=holdup+1
-                if(dp<15):
+                if(dp<15): #go to finish state
                     state=3
-            elif(state==3):
+            elif(state==3):# finish rep state
                 curls=curls+1
                 print("curl number",curls,"held for",holdup/10,"seconds")
-                state=1
+                state=1 # go back to down state for next rep
                     
-                
+            # reset collection of values used to calculate average  over .1 seconds
             yaws=[]
             pitches=[]
             rolls=[]
