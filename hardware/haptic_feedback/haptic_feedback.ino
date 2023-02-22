9c1521310b4ab5d9e72170b1e7a658dcd7cec842
ArturoGO28 <arturog@bu.edu>
2023-02-22 15:54:57 -0500

    haptic

--- hardware/haptic_feedback/haptic_feedback.ino
+++ hardware/haptic_feedback/haptic_feedback.ino
@@ -1,102 +1,139 @@
+#include <Arduino_LSM9DS1.h>
+#include <Adafruit_AHRS.h>
 #include <ArduinoBLE.h>
+#define sensorRate 104.00
+Adafruit_Mahony filter;
 
-BLEService ledService("180A"); // BLE LED Service
+BLEService AHRSfilter("A123"); // create service
+BLEFloatCharacteristic RollCharacteristic("2A19", BLERead | BLENotify); //create Characteristic
+BLEFloatCharacteristic PitchCharacteristic("2A20", BLERead | BLENotify); //create Characteristic
+BLEFloatCharacteristic HeadingCharacteristic("2A21", BLERead | BLENotify); //create Characteristic
 
-// BLE LED Switch Characteristic - custom 128-bit UUID, read and writable by central
+BLEService HapticFeedback("A124");
 BLEByteCharacteristic switchCharacteristic("2A57", BLERead | BLEWrite);
 
+//PINS
+#define powerdrain 12
+#define haptic 11
+
+unsigned long start_time;
+<<<<<<< Updated upstream
+char* deviceName = "SmartGymBros_kidney";
+=======
+bool hapticState = true;
+>>>>>>> Stashed changes
+
 void setup() {
-  Serial.begin(9600);
-  while (!Serial);
+  pinMode(powerdrain, OUTPUT);
+  pinMode(haptic, OUTPUT);
+  digitalWrite(powerdrain, LOW);
+  digitalWrite(haptic, LOW);
 
-  // set LED's pin to output mode
-  pinMode(LEDR, OUTPUT);
-  pinMode(LEDG, OUTPUT);
-  pinMode(LEDB, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
-
-  digitalWrite(LED_BUILTIN, LOW);         // when the central disconnects, turn off the LED
-  digitalWrite(LEDR, HIGH);               // will turn the LED off
-  digitalWrite(LEDG, HIGH);               // will turn the LED off
-  digitalWrite(LEDB, HIGH);                // will turn the LED off
-
-  // begin initialization
-  if (!BLE.begin()) {
-    Serial.println("starting Bluetooth® Low Energy failed!");
-
-    while (1);
+  // attempt to start the IMU:
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU");
+    // stop here if you can't access the IMU:
+    while (true);
   }
+  // Calibration:
+  // Gyroscope code
+   IMU.setGyroFS(2);
+   IMU.setGyroODR(5);
+   IMU.setGyroOffset (0.202271, -0.971649, 0.442047);
+   IMU.setGyroSlope (1.202876, 1.170078, 1.419463);
+  // Magnetometer code
+   IMU.setMagnetFS(0);
+   IMU.setMagnetODR(8);
+   IMU.setMagnetOffset(-10.064087, 18.947754, 18.128662);
+   IMU.setMagnetSlope (1.145179, 1.314404, 1.053118);
+  // Accelerometer code
+   IMU.setAccelFS(3);
+   IMU.setAccelODR(5);
+   IMU.setAccelOffset(0.003756, -0.012898, 0.019816);
+   IMU.setAccelSlope (0.989642, 0.994564, 0.977860);
+   
+  // start the filter to run at the sample rate:
+  filter.begin(sensorRate);
+  Serial.print("Orientation:\n");
 
-  // set advertised local name and service UUID:
-  BLE.setLocalName("Nano 33 BLE");
-  BLE.setAdvertisedService(ledService);
-
-  // add the characteristic to the service
-  ledService.addCharacteristic(switchCharacteristic);
-
-  // add service
-  BLE.addService(ledService);
-
-  // set the initial value for the characteristic:
-  switchCharacteristic.writeValue(0);
-
-  // start advertising
+  // BLE Setup  
+  BLE.begin();
+  BLE.setLocalName(deviceName);
+  BLE.setAdvertisedService(AHRSfilter);
+  AHRSfilter.addCharacteristic(RollCharacteristic);
+  AHRSfilter.addCharacteristic(PitchCharacteristic);
+  AHRSfilter.addCharacteristic(HeadingCharacteristic);
+  BLE.addService(AHRSfilter);
   BLE.advertise();
 
-  Serial.println("BLE LED Peripheral");
+  HapticFeedback.addCharacteristic(switchCharacteristic);
+  BLE.addService(HapticFeedback);
+
+  // Serial
+  Serial.begin(9600);
+  //temporary counter
+  start_time = millis();
+
+  pinMode(LEDR, OUTPUT);
+  digitalWrite(LEDR, HIGH);
 }
 
 void loop() {
-  // listen for BLE peripherals to connect:
+  //power control---------------
+  unsigned long nowtime=millis();
+  
+  if ( nowtime - start_time >= 10000)
+    digitalWrite(powerdrain, HIGH);
+ 
+  if(nowtime%1000 < 400)
+    digitalWrite(haptic,HIGH);
+  else
+    digitalWrite(haptic,LOW);
+  //----------------------------
+  
   BLEDevice central = BLE.central();
+  
+  //Output Values
+  float roll, pitch, heading;
+  //IMU values
+  float xAcc, yAcc, zAcc,
+        xGyro,yGyro,zGyro,
+        xMag, yMag, zMag;
+  if(IMU.accelerationAvailable() && 
+     IMU.gyroscopeAvailable() &&
+     IMU.magneticFieldAvailable()
+     ){
+    IMU.readAcceleration(xAcc, yAcc, zAcc);
+    IMU.readGyroscope(xGyro, yGyro, zGyro);
+    IMU.readMagneticField(xMag, yMag, zMag);
+  }
+  //update filter
+  filter.update(xGyro,yGyro,-zGyro,xAcc,yAcc,-zAcc,-xMag,yMag,-zMag);
+  //get filter output:
+  roll = filter.getRoll();
+  pitch = filter.getPitch();
+  heading = filter.getYaw();
+  //print values
+  Serial.print(deviceName);
+  Serial.print("\theading: ");
+  Serial.print(heading);
+  Serial.print("\tpitch: ");
+  Serial.print(pitch);
+  Serial.print("\troll: ");
+  Serial.println(roll);
 
-  // if a central is connected to peripheral:
+  RollCharacteristic.writeValue(roll);
+  PitchCharacteristic.writeValue(pitch);
+  HeadingCharacteristic.writeValue(heading);
+
   if (central) {
-    Serial.print("Connected to central: ");
-    // print the central's MAC address:
-    Serial.println(central.address());
-    digitalWrite(LED_BUILTIN, HIGH);            // turn on the LED to indicate the connection
-
-    // while the central is still connected to peripheral:
     while (central.connected()) {
-      // if the remote device wrote to the characteristic,
-      // use the value to control the LED:
-      if (switchCharacteristic.written()) {
-        switch (switchCharacteristic.value()) {   // any value other than 0
-          case 01:
-            Serial.println("Red LED on");
-            digitalWrite(LEDR, LOW);            // will turn the LED on
-            digitalWrite(LEDG, HIGH);         // will turn the LED off
-            digitalWrite(LEDB, HIGH);         // will turn the LED off
-            break;
-          case 02:
-            Serial.println("Green LED on");
-            digitalWrite(LEDR, HIGH);         // will turn the LED off
-            digitalWrite(LEDG, LOW);        // will turn the LED on
-            digitalWrite(LEDB, HIGH);        // will turn the LED off
-            break;
-          case 03:
-            Serial.println("Blue LED on");
-            digitalWrite(LEDR, HIGH);         // will turn the LED off
-            digitalWrite(LEDG, HIGH);       // will turn the LED off
-            digitalWrite(LEDB, LOW);         // will turn the LED on
-            break;
-          default:
-            Serial.println(F("LEDs off"));
-            digitalWrite(LEDR, HIGH);          // will turn the LED off
-            digitalWrite(LEDG, HIGH);        // will turn the LED off
-            digitalWrite(LEDB, HIGH);         // will turn the LED off
-            break;
-        }
+      if (switchCharacteristic.written() && hapticState) {
+        digitalWrite(LEDR, LOW);
+      } else {
+        digitalWrite(LEDR, HIGH);        
       }
     }
-
-    // when the central disconnects, print it out:
-    Serial.print(F("Disconnected from central: "));
-    Serial.println(central.address());
-    digitalWrite(LED_BUILTIN, LOW);         // when the central disconnects, turn off the LED
-    digitalWrite(LEDR, HIGH);          // will turn the LED off
-    digitalWrite(LEDG, HIGH);        // will turn the LED off
-    digitalWrite(LEDB, HIGH);         // will turn the LED off
   }
+
 }
